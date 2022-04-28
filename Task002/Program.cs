f94826a1f4648eae3f2e7ce4ce30a17890e73ff6
Tamara <tomritol@gmail.com>
2022-04-28 20:06:26 +0300

    Задача 2

---
+++ Task002/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на
+// вход три числа и выдаёт максимальное из этих чисел.
+
+int numA = int.Parse(Console.ReadLine());
+int numB = int.Parse(Console.ReadLine());
+int numC = int.Parse(Console.ReadLine());
+
+int max = numA;
+if (numB > max)  max = numB;
+if (numC > max)  max = numC;
+
+Console.Write("Максимальное из этих чисел - ");
+Console.WriteLine(max);
