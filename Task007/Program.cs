85d5811bd344b190761eb7468145d9363b8528a4
Tamara <tomritol@gmail.com>
2022-04-29 19:01:54 +0300

    задача 007

--- Task007/Program.cs
+++ Task007/Program.cs
@@ -2,7 +2,7 @@
 // трёхзначное число и на выходе показывает последнюю
 // цифру этого числа.
 
-Console.Write("Введи трехзначное число ");
+Console.Write("Введите трехзначное число ");
 int N = int.Parse(Console.ReadLine());
 double lastnumber = N%10;
 
