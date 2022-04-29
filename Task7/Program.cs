6d915735036070d5a1ae037899e16ed0010023f7
Tamara <tomritol@gmail.com>
2022-04-29 19:21:15 +0300

    задача 7

---
+++ Task7/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю
+// цифру этого числа.
+
+Console.Write("Введите трехзначное число - ");
+int N = int.Parse(Console.ReadLine());
+double lastnumber = N%10;
+
+  if(lastnumber<0)
+    {
+         lastnumber=-lastnumber;
+    }
+ 
+Console.WriteLine("Последняя цифра числа - " + lastnumber);
+
+
